8bfb3bb56a6003d2fa3e3aea43ceced5891bec58
Xers9 <xers989@gmail.com>
2021-12-16 15:50:03 +0900

    Update index.js

--- mongo/index.js
+++ mongo/index.js
@@ -6,7 +6,7 @@ const dbName = 'test';
 
 const MongoClient = require('mongodb').MongoClient;
 //mongodb://admin:Rbehd1768@10.0.0.6/test?retryWrites=true&w=majority
-const client = new MongoClient('mongodb://admin:Rbehd1768@mongo.myjson.site:27001,mongo.myjson.site:27002,mongo.myjson.site:27003/test?authSource=admin&directConnection=true&retryWrites=true&w=majority&ReadPreference=primary', {
+const client = new MongoClient('mongodb://admin:****@mongo.myjson.site:27001,mongo.myjson.site:27002,mongo.myjson.site:27003/test?authSource=admin&directConnection=true&retryWrites=true&w=majority&ReadPreference=primary', {
   tlsCAFile: 'tls/example-ca-pub.crt',
   tlsCertificateKeyFile: 'tls/example-client.pem',
   tlsAllowInvalidCertificates: true
